2c4e06efb9564e8d2ae05a285de86ba36b5daa8b
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-01-28 16:24:23 +0100

    feat: add cache update

--- src/cache/main.py
+++ src/cache/main.py
@@ -4,6 +4,7 @@ cache
 
 # pylint: disable=no-method-argument, too-few-public-methods
 
+import fileinput
 from os import path
 from shutil import copyfile
 
@@ -13,6 +14,16 @@ class Cache:
 
     def _exists(self) -> bool:
         return path.isfile("sushicache.py")
+
+    def update(old: str, new: str):
+        """
+        overwrites old cache data with new one
+        """
+
+        with fileinput.input("sushicache.py", inplace=True) as f:
+            for line in f:
+                new_line = line.replace(old, new)
+                print(new_line, end="")
 
     def _copy_template(self) -> None:
         copyfile("src/cache/cache_template", "sushicache.py")
