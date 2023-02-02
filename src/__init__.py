b7c482340051ecdb8c38d24794cf0b7fc6cbdd20
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-02-02 17:40:55 +0100

    feat: check if config should be extended

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,21 @@
 sushi
 """
 
+import configparser
 import sys
 from os import path
+
+from src.config.extends import ConfigExtends
+
+config = configparser.ConfigParser()
+config.read("sushi.conf")
 
 from rich import print as rich_print
 
 if not path.isfile("sushi.conf"):
     rich_print("[bold red]sushi[/bold red]   configuration file doesnt exists")
     sys.exit(1)
+
+# check if configs needs to be extended
+if config["extends"]["repo"] is not None:
+    ConfigExtends.install(ConfigExtends)
