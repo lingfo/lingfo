72943ebf66f59ca75cf2ae424ca47146d282c182
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-02-17 20:27:21 +0100

    feat: add arg-cpp example

---
+++ examples/arg-cpp/app.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-module-docstring
+
+# from out.main import helloWorld
+from sushipy.main import Sushi
+
+if __name__ == "__main__":
+    Sushi()
+
+    # after first launch, we can call function from c++
+    # helloWorld(1)
---
+++ examples/arg-cpp/lib/main.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void helloWorld(int number)
+{
+    std::cout << "You provided " << number << "\n";
+}
