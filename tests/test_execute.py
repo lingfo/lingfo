1ad118fe4398f2f57450f1ac110cd34d1ba0968f
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-02-25 13:03:24 +0100

    fix: change python version for subprocess.run

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -33,7 +33,7 @@ class TestExecute:
         os.chdir("noarg-temp/")
 
         result = subprocess.run(
-            ["python3.9", "app.py"], capture_output=True, text=True, check=False
+            ["python", "app.py"], capture_output=True, text=True, check=False
         )
         result = result.stdout.split("\n")
 
