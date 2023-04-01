364112dde262b8b140be15ca313ada989114e011
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-04-01 11:57:00 +0200

    fix: lint and test errors

--- src/sushipy/git.py
+++ src/sushipy/git.py
@@ -1,8 +1,10 @@
 """Tracks library change with git"""
 
+import configparser
 import subprocess
+import sys
 from contextlib import suppress
-from os import chdir, makedirs, name, rename, system
+from os import chdir
 from os.path import isfile
 from sys import platform
 
@@ -16,8 +18,13 @@ if isfile("sushicache.py"):
     import sushicache
 # pylint: enable=import-error
 
+config = configparser.ConfigParser()
+config.read("sushi.conf")
+
 
 class GitTracking:
+    """git tracking"""
+
     def _find_changes(self) -> None:
         """returns false if no changes"""
 
@@ -26,22 +33,23 @@ class GitTracking:
             ["git", "--git-dir=.sushi-git", "status"],
             capture_output=True,
             text=True,
+            check=True,
         )
         result = result.stdout.split("\n")
 
-        if result[1] == "nothing to commit, working tree clean":
-            return False
-        else:
-            return True
+        return result[1] == "nothing to commit, working tree clean"
 
     def _configure_git(self) -> None:
-        rich_print(
-            "To continue, sushi needs to execute one shell script. If you agree to continue, press any key. Otherwise press N"
-        )
+        with suppress(KeyError):
+            if config.getboolean("main", "trusted") is False:
+                rich_print(
+                    "To continue, sushi needs to execute one shell script. "
+                    + "If you agree to continue, press any key. Otherwise press N"
+                )
 
-        input_data = input()
-        if input_data.capitalize() == "N":
-            exit(0)
+                input_data = input()
+                if input_data.capitalize() == "N":
+                    sys.exit(0)
 
         def v_print(script_name: str):
             verbose_print(
@@ -49,7 +57,7 @@ class GitTracking:
             )
 
         # run script
-        if platform == "linux" or platform == "linux2" or platform == "darwin":
+        if platform in ("linux", "linux2", "darwin"):
             v_print("git_init.sh")
             subprocess.call(
                 ["src/sushipy/scripts/git_init.sh", "lib"],
@@ -64,7 +72,7 @@ class GitTracking:
 
     def __init__(self) -> None:
         with suppress(NameError):
-            if sushicache.GIT_CONFIGURED == True:
+            if sushicache.GIT_CONFIGURED is True:
                 return
 
         self._configure_git()
