8b9e52662b1408ba56d14f63bcd91ee6081fcedc
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-04-14 17:55:44 +0200

    feat: add simple parsing using tree-sitter

---
+++ src/sushipy/config/auto_detect.py
@@ -0,0 +1,74 @@
+import os
+from os.path import isfile
+
+from git import Repo
+from rich import print as rich_print
+from tree_sitter import Language, Parser
+
+from ..cache.main import Cache
+
+# pylint: disable=import-error
+if isfile("sushicache.py"):
+    import sushicache
+# pylint: enable=import-error
+
+
+class TSDetect:
+    """detect using TreeSitter"""
+
+    def setup(self, file_path, language):
+        rich_print(
+            "[bold green]sushi[/bold green]    configuring tree-sitter for your language"
+        )
+
+        if not os.path.exists(".sushi"):
+            os.makedirs(".sushi")
+
+        # first, download parser for selected language
+        Repo.clone_from(
+            f"https://github.com/tree-sitter/tree-sitter-{language}",
+            file_path + "source/",
+        )
+
+        # and build library for tree-sitter
+        Language.build_library(
+            file_path + f"build/{language}.so", [file_path + "source/"]
+        )
+
+        Cache.update(
+            Cache, "TREE_SITTER_CONFIGURED = False", "TREE_SITTER_CONFIGURED = True"
+        )
+
+    def __init__(self, language: str) -> None:
+        file_path = f".sushi/tree-sitter-{language}/"
+
+        # setup tree sitter
+        if sushicache.TREE_SITTER_CONFIGURED == False:
+            self.setup(file_path, language)
+
+        CPP_LANGUAGE = Language(file_path + f"build/{language}.so", "cpp")
+
+        # create new parser
+        parser = Parser()
+        parser.set_language(CPP_LANGUAGE)
+
+        self.tree = parser.parse(
+            bytes(
+                """
+            #include <iostream>
+
+            void helloWorld() {
+                std::cout << "hello world\n";
+            }
+            """,
+                "utf8",
+            )
+        )
+
+        self.parse_tree()
+
+    def parse_tree(self):
+        tree = self.tree
+
+        for node in tree.root_node.children:
+            print(node.type)
